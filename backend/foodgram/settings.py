bb419e5fe51f9baaa46ac076c49fed98cc11ca7d
nu-shtosh <i9858955749@ya.ru>
2022-01-11 14:45:02 +0300

    vuala

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Секретики
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
